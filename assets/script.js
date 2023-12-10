3042119df5bc322ad25cb37da30a6a55826c1819
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2023-12-10 16:58:57 -0500

    added function method to update score

--- assets/script.js
+++ assets/script.js
@@ -112,104 +112,12 @@ let questions = [
     },
 ];
 
-    /*
-    {
-        title: "Which of the following is a useful method to iterate over items inside an array?",
-        answer: "for loop",
-        options: [
-            "do...while",
-            "concatenation",
-            "recursive function"
-        ]
-    },
-    {
-        title: "Which scope will hold a variable declared inside a function?",
-        answer: 2,
-        options: [
-            "global",
-            "neighbor",
-            "local",
-            "home"
-        ]
-    },
-    {
-        title: "The condition in a if/else statement is enclosed inside:",
-        answer: 3,
-        options: [
-            "square brackets",
-            "parentheses",
-            "quotes",
-            "curly brackets"
-        ]
-    },
-    {
-        title: "Where do you link to a Javascript file inside the HTML document?",
-        answer: 0,
-        options: [
-            "inside the body but at the bottom"
-            "at the top",
-            "inside the head element",
-            "inside the footer"
-        ]
-    },
-    {
-        title: "Arrays in Javascript can be used to store:",
-        answer: 3,
-        options: [
-            "strings",
-            "numbers",
-            "booleans",
-            "all of the above"
-        ]
-    },
-    {
-        title: "What does DOM stand for?",
-        answer: 2,
-        options: [
-            "Download Order Mechanism",
-            "Deputy Of Movement",
-            "Document Object Model",
-            "Department of Music"
-        ]
-    },
-    {
-        title: "What does the triple equals symbol mean?",
-        answer: 3,
-        options: [
-            "equality operator",
-            "not equal operator",
-            "assignment operator",
-            "strict equality operator"
-        ]
-    },
-    {
-        title: "Which of the following is helpful for debugging code while using the browser:",
-        answer: 0,
-        options: [
-            "the console",
-            "vs code editor",
-            "terminal",
-            "github"
-        ]
-    },
-    {
-        title: "Which of the following can be used to declare a variable?",
-        answer: 
-        options: [
-            "var",
-            "let",
-            "const",
-            "all of the above",
-        ]
-    },
-];
-
-*/
 
 let quizApp = {
     startQuiz: function() {
         
         this.currentPosition = 0;
+        this.score = 0;
 
         // get options to quiz question
         let options = document.querySelectorAll('.options');
@@ -228,23 +136,24 @@ let quizApp = {
             });
         });
 
-    // show first question
-    this.showQuestion(questions[this.currentPosition]);
+            // update score
+            this.updateScore();
+
+            // show first question
+            this.showQuestion(questions[this.currentPosition]);
     },
 
     showQuestion: function(q) {
 
         // select title element
         let titleDiv = document.getElementById('title');
-
-        // modify title element
         titleDiv.textContent = q.title;
 
+        // get question options
         let options = document.querySelectorAll('.options');
     
         options.forEach(function(element, index) {
         element.textContent = q.options[index];
-
         });
     },
 
@@ -252,14 +161,18 @@ let quizApp = {
 
         let currentQuestion = questions[this.currentPosition];
 
-        if (currentQuestion.answer == userSelect ) {
+        if (currentQuestion.answer == userSelect) {
             // correct answer
             console.log('correct');
+            this.score++;
         } 
         else {
             // wrong answer
             console.log('incorrect');
         }
+        
+        // update score
+        this.updateScore();
 
         // increase currentPosition
         this.increasePosition();
@@ -274,9 +187,12 @@ let quizApp = {
         if (this.currentPosition == questions.length) {
             this.currentPosition = 0;
         }
+    },
+
+    updateScore: function() {
+        let scoreDiv = document.getElementById('score');
+        scoreDiv.textContent = 'Score: ' + this.score;
     }
 };
-
-
 
 quizApp.startQuiz();
