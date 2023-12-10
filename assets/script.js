fce79bde662f9116ea3f5640c87d0927f8fb6938
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2023-12-09 21:08:09 -0500

    added quiz questions to script.js

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        id: 1,
+        question: "Which of the following is NOT a commonly used data type?",
+        answer: "alerts",
+        options: [
+            "booleans",
+            "strings",
+            "numbers"
+        ]
+    },
+    {
+        id: 2,
+        question: "Which of the following is a useful method to iterate over items inside an array?",
+        answer: "for loop",
+        options: [
+            "do...while",
+            "concatenation",
+            "recursive function"
+        ]
+    },
+    {
+        id: 3,
+        question: "Which scope will hold a variable declared inside a function?",
+        answer: "local",
+        options: [
+            "global",
+            "neighbor",
+            "home"
+        ]
+    },
+    {
+        id: 4,
+        question: "The condition in a if/else statement is enclosed inside: ",
+        answer: "curly brackets",
+        options: [
+            "square brackets",
+            "parentheses",
+            "quotes"
+        ]
+    },
+    {
+        id: 5,
+        question: "Where do you link to a Javascript file inside the HTML document?",
+        answer: "inside the body but at the bottom",
+        options: [
+            "at the top",
+            "inside the head element",
+            "inside the footer"
+        ]
+    }
+]
