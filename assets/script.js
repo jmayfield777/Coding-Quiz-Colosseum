3921f25f25e5b1dce1037cdd18e50d7feaa8be77
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2023-12-09 21:28:53 -0500

    added 5 more questions to script.js

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,7 @@ let questions = [
     },
     {
         id: 4,
-        question: "The condition in a if/else statement is enclosed inside: ",
+        question: "The condition in a if/else statement is enclosed inside:",
         answer: "curly brackets",
         options: [
             "square brackets",
@@ -48,5 +48,55 @@ let questions = [
             "inside the head element",
             "inside the footer"
         ]
-    }
-]
+    },
+    {
+        id: 6,
+        question: "Arrays in Javascript can be used to store:",
+        answer: "all of the above",
+        options: [
+            "strings",
+            "numbers",
+            "booleans"
+        ]
+    },
+    {
+        id: 7,
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Download Order Mechanism",
+            "Deputy Of Movement",
+            "Department of Music"
+        ]
+    },
+    {
+        id: 8,
+        question: "What does the triple equals symbol mean?",
+        answer: "strict equality operator",
+        options: [
+            "equality operator",
+            "not equal operator",
+            "assignment operator"
+        ]
+    },
+    {
+        id: 9,
+        question: "Which of the following is helpful for debugging code while using the browser:",
+        answer: "the console",
+        options: [
+            "vs code editor",
+            "terminal",
+            "github"
+        ]
+    },
+    {
+        id: 10,
+        question: "Which of the following can be used to declare a variable?",
+        answer: "all of the above",
+        options: [
+            "var",
+            "let",
+            "const"
+        ]
+    },
+];
