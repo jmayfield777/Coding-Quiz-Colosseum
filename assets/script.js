4b02790a593ad601905de335e17cfe2abd92fe18
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2023-12-11 12:33:28 -0500

    added quiz questions to script.js

---
+++ assets/script.js
@@ -0,0 +1,102 @@
+let questions = [
+    {
+      prompt: "Which of the following is NOT a commonly used data type?",
+      options: [
+        "booleans",
+        "strings",
+        "numbers",
+        "alerts"
+      ],
+      answer: "alerts"
+    },
+    {
+      prompt: "Which of the following is a useful method to iterate over items inside an array?",
+      options: [
+        "do...while",
+        "for loop",
+        "concatenation",
+        "recursive function"
+       ],
+       answer: "for loop"
+    },
+    {
+      prompt: "Which scope will hold a variable declared inside a function?",
+      options: [
+        "global",
+        "neighbor",
+        "local",
+        "home"
+      ],
+      answer: "local"
+    },
+    {
+      prompt: "The condition inside an if/else statement is enclosed inside:",
+      options: [
+        "quotes",
+        "square brackets",
+        "curly brackets",
+        "parenthesis"
+      ],
+      answer: "curly brackets"
+    },
+    {
+      prompt: "Where do you usually link a Javascript file inside the HTML document?",
+      options: [
+        "inside the body but at the bottom",
+        "at the top",
+        "inside the head element",
+        "inside the footer"
+      ],
+      answer: "inside the body but at the bottom"
+    },
+    {
+      prompt: "Arrays in Javascript can be used to store:",
+      options: [
+        "strings",
+        "numbers",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      prompt: "What does DOM stand for?",
+      options: [
+        "Download Order Mechanism",
+        "Deputy of Movement",
+        "Document Object Model",
+        "Department of Music"
+      ],
+      answer: "Document Object Model"
+    },
+    {
+      prompt: "What does the triple equals symbol mean?",
+      options: [
+        "equality operator",
+        "not equal operator",
+        "assignment operator",
+        "strict equality operator"
+      ],
+      answer: "strict equality operator"
+    },
+    {
+      prompt: "Which of the following is helpful for debugging code while working in a browser?",
+      options: [
+        "the console",
+        "vs code editor",
+        "terminal",
+        "github"
+      ],
+      answer: "the console"
+    },
+    {
+      prompt: "Which of the following can be used to declare a variable?",
+      options: [
+        "var",
+        "let",
+        "const",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    }
+];
